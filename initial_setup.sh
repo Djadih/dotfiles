186a230984ac36653f05182866a7fb3629588b03
hussam-u410 <hussam@unit410.com>
2025-10-11 16:31:39 -0500

    add auto install script

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,8 @@
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y zsh tmux vim magic-wormhole
+sudo apt install -y zsh git tmux vim magic-wormhole
 
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/Djadih/dotfiles.git ~/dotfiles && cd ~/dotfiles && cp -rf .[^.]* ~/
 
