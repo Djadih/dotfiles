95d4d40047db05274c0bdfe8c6ae3a90f88c9aa8
hussam-u410 <hussam@unit410.com>
2025-10-11 16:49:43 -0500

    lock down ssh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,7 @@
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y zsh git tmux vim magic-wormhole
+sudo apt install -y zsh git tmux vim magic-wormhole ssh
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config && sudo systemctl restart sshd
 
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
