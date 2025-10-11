24dbf70fc6aa45990f34c7cc8cd00f50a2f7dd74
hussam-u410 <hussam@unit410.com>
2025-10-11 16:21:27 -0500

    initial setup script

---
+++ initial_setup.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y zsh tmux vim magic-wormhole
+
+RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
