f48cee654aeec0d72a77b3e931ccbd5ac5f6afd5
hussam-u410 <hussam@unit410.com>
2025-12-31 13:12:58 -0600

    install fzf on startup

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y zsh git tmux vim magic-wormhole ssh
+sudo apt install -y zsh git tmux vim magic-wormhole ssh fzf
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config && sudo systemctl restart sshd
 
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
