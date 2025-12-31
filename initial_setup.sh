71b0a6a016ec748a2cdc22ca03fa7f582bbd58b1
hussam-u410 <hussam@unit410.com>
2025-12-31 13:17:06 -0600

    change shell to zsh

--- initial_setup.sh
+++ initial_setup.sh
@@ -4,6 +4,7 @@ sudo apt install -y zsh git tmux vim magic-wormhole ssh fzf
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config && sudo systemctl restart sshd
 
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo chsh -s "$(command -v zsh)" "$USER"
 
 git clone https://github.com/Djadih/dotfiles.git ~/dotfiles && cd ~/dotfiles && cp -rf .[^.]* ~/
 
