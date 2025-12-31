2a4c8619eed1406aa870ba84d6f98f7c60503859
hussam-u410 <hussam@unit410.com>
2025-12-31 13:19:54 -0600

    install omz plugins

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,17 @@
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y zsh git tmux vim magic-wormhole ssh fzf
+sudo apt install -y \
+  zsh \
+  git \
+  tmux \
+  vim \
+  ssh \
+  magic-wormhole \
+  fzf \
+  zsh-autosuggestions \
+  zsh-syntax-highlighting \
+  zsh-completions
+
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config && sudo systemctl restart sshd
 
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
